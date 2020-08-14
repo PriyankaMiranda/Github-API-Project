ad6374b8e2300c5f6db9098672c09ba2f8a4e26f
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-08-14 17:34:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+const server = app.listen(port, () => {
+	console.log("Server is running at port " + port);
+});
+
+
+
+
+
+
+
+
+

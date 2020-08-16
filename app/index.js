624b6acc4694ad926a995dbe2dbe6dcc3fd46d1a
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-08-16 17:47:25 +0530

    getting forks to repo

--- app/index.js
+++ app/index.js
@@ -2,10 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 class App extends React.Component{
+    constructor(props){
+		super(props);
+		this.state = {
+			data : ''
+    	}
+    	this.dataHandler = this.dataHandler.bind(this);
+  	}
+
+	componentDidMount() {
+		var owner = 'facebook'
+		var repo = 'react'
+		const url = `https://api.github.com/repos/${owner}/${repo}/forks`;
+		// Providing 3 arguments (GET/POST, The URL, Async True/False)
+		// xhr.open('GET', url, true);
+
+		fetch(url).then(res => {
+		  return res.json()
+		}).then(res => {
+		    this.setState({ data: res});
+		}).catch(console.log("error"));
+
+  	}
+
+  	dataHandler (data){this.setState({data:data})}
+
     render(){
+    	console.log(this.state.data)
         return(
-            <div>Data</div>
+            <div>{this.state.data}</div>
         )
     }
 }
